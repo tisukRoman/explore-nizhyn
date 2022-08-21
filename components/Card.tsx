18ffdfbb2dfee03701d3f5aa51a23c2bb817effc
tisukRoman <tisukroman@gmail.com>
2022-08-21 22:38:21 +0300

    fetching data from supabase and text-align inside Card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -11,13 +11,14 @@ const Card: FC<CardProps> = ({ post }) => {
   return (
     <div className='h-128 w-full bg-gray-200 overflow-hidden relative group first:col-span-2'>
       <Image
+        unoptimized
         src={post.img_src || '/images/placeholder.png'}
         alt={post.title}
         layout='fill'
         className='object-cover w-full h-full z-0 drop-shadow-2xl lg:grayscale group-hover:grayscale-0 group-hover:scale-110 transition-all duration-700'
       />
       <div className='bg-black w-full h-full z-1 flex flex-col items-center justify-center absolute bg-opacity-60 shadow-2xl'>
-        <h3 className='text-white font-medium uppercase text-3xl hover-green'>
+        <h3 className='text-white font-medium uppercase text-3xl hover-green text-center max-w-[80%] mx-auto'>
           {post.title}
         </h3>
         <p className='text-slate-200 text-base my-2 uppercase hover-green'>
