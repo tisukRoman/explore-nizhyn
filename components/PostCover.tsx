51a1c62b230ae1cfea10856860b846a1e38df233
tisukRoman <tisukroman@gmail.com>
2022-08-26 16:49:06 +0300

    added description for PostCover

--- components/PostCover.tsx
+++ components/PostCover.tsx
@@ -1,31 +1,28 @@
-import Image from 'next/image';
 import { FC } from 'react';
+import Image from 'next/image';
+import { PostDetails } from '../utils/types';
 
-type PostCoverProps = {
-  title: string;
-  created_at: string;
-  img_src: string;
-  tag: string;
-};
-
-const PostCover: FC<PostCoverProps> = (props) => {
+const PostCover: FC<{ post: PostDetails }> = ({ post }) => {
   return (
     <div className='w-full h-128 bg-[#000] relative'>
       <Image
         unoptimized
-        src={props.img_src}
-        alt={props.title}
+        src={post.img_src ? post.img_src : '/images/placeholder.png'}
+        alt={post.title}
         layout='fill'
         className='h-full object-cover opacity-60'
       />
       <div className='bg-black w-full h-full z-1 flex flex-col items-center justify-center absolute bg-opacity-60 shadow-2xl'>
         <h3 className='text-white font-medium uppercase text-3xl hover-green text-center max-w-[80%] mx-auto'>
-          {props.title}
+          {post.title}
         </h3>
         <p className='text-slate-200 text-base my-2 uppercase hover-green'>
-          {props.tag}
+          {post.tag}
         </p>
         <div className='bg-green w-10 my-3 h-1' />
+        <div className='text-slate-200 my-2 mx-auto text-center max-w-[80%] md:max-w-[40%]'>
+          {post.description}
+        </div>
       </div>
     </div>
   );
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -15,12 +15,7 @@ const PostDetails: NextPage<PostDetailsProps> = ({ post, comments }) => {
     <>
       <Header />
       <main className='pt-20 mx-auto lg:max-w-screen-lg shadow-xl shadow-[#1a1a1a]'>
-        <PostCover
-          title={post.title || 'title'}
-          created_at={post.created_at || 'created_at'}
-          img_src={post.img_src || '/images/placeholder.png'}
-          tag={post.tag || 'tag'}
-        />
+        <PostCover post={post} />
         <article className='text-slate-300 text-lg px-8 py-12 font-mono md:text-2xl'>
           <p className='leading-8 md:leading-10'>{post.content}</p>
         </article>
