b9397df526eeda491e95139f4ac6be3193bc5ca4
tisukRoman <tisukroman@gmail.com>
2022-10-05 16:05:41 +0300

    disabled style for TextInput

--- src/components/shared/TextInput.tsx
+++ src/components/shared/TextInput.tsx
@@ -12,11 +12,13 @@ const TextInput = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
       <input
         ref={ref}
         {...props}
-        className={`${
-          has_error
-            ? 'border-b-[1px] border-red-500 text-red-500 placeholder:text-red-500'
-            : ''
-        } w-full text-white placeholder:text-slate-500 outline-none bg-black p-4 mt-4 focus:border-b-[1px] focus:border-slate-500`}
+        className={`
+        ${
+          has_error &&
+          'border-b-[1px] border-red-500 text-red-500 placeholder:text-red-500'
+        } 
+        ${props.disabled && 'opacity-70 bg-slate-400'}
+        w-full text-white placeholder:text-slate-500 outline-none bg-black p-4 mt-4 focus:border-b-[1px] focus:border-slate-500 appearance-none`}
       />
       {has_error ? <div className='text-red-500 p-2'>{error_text}</div> : null}
     </>
